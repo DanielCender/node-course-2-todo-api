326297bde73607247adfa298157cc03114fbe38b
Daniel Cender <danielcender@Daniels-MacBook-Pro.local>
2018-06-27 19:42:58 -0500

    Add testing for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,16 +1,20 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 const todos = [
 	{
+		_id: new ObjectID(),
 		text: "First test todo",
 	},
 	{
+		_id: new ObjectID(),
 		text: "Second test todo",
 	},
 	{
+		_id: new ObjectID(),
 		text: "Third test todo",
 	},
 ];
@@ -75,6 +79,34 @@ describe("GET /todos", () => {
 			.expect(res => {
 				expect(res.body.length).toBe(3);
 			})
+			.end(done);
+	});
+});
+
+describe("GET /todos", () => {
+	it("should return todo doc", done => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+	});
+
+	it("should return a 404 if todo not found", done => {
+		let hexID = new ObjectID().toHexString();
+		// Make req using real ObjID but not linked to obj
+		request(app)
+			.get(`/todos/${hexID}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it("should return 404 for non-object ids", done => {
+		request(app)
+			.get("/todos/12345")
+			.expect(404)
 			.end(done);
 	});
 });
